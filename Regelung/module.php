591946b9acc6f7e7745d3b3bdee7fde98602b2f4
minor04 <46103212+minor04@users.noreply.github.com>
2019-02-10 09:47:18 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -37,10 +37,9 @@ class ALGModus extends IPSModule
 			if (!IPS_VariableProfileExists("ALG_Akt")) {
 			
 				IPS_CreateVariableProfile("ALG_Akt", 0); // 0 boolean, 1 int, 2 float, 3 string,
-				//IPS_SetVariableProfileValues("ALG_Programm", 1, 3, 0);
-				//IPS_SetVariableProfileDigits("ALG_Programm", 0);
-				IPS_SetVariableProfileAssociation("ALG_Akt", false, "Deaktiv", "", 0xFFFFFF);
-				IPS_SetVariableProfileAssociation("ALG_Akt", true, "Aktiv", "", 0xFFFFFF);
+				IPS_SetVariableProfileIcon("ALG_Akt",  "Switch");
+				IPS_SetVariableProfileAssociation("ALG_Akt", false, "Deaktiv", "", 0x00FF00);
+				IPS_SetVariableProfileAssociation("ALG_Akt", true, "Aktiv", "", 0x00FF00);
 				
 			}
 		
@@ -48,9 +47,9 @@ class ALGModus extends IPSModule
 			//___In_IPS_zurverfügungstehende_Variabeln_______________________________________________
 			$this->RegisterVariableInteger("Mod", "Modus", "ALG-Modus", 1);
 			$this->RegisterVariableInteger("Prog", "Programm", "ALG_Programm", 2);
-			$this->RegisterVariableBoolean("HZ", "Heizung Abwesend", "~Switch", 5);
+			$this->RegisterVariableBoolean("HZ", "Heizung Abwesend", "ALG_Akt", 5);
 			$this->RegisterVariableBoolean("MD", "Meldung", "ALG_Akt", 6);
-			$this->RegisterVariableBoolean("ZP", "AutoZSP", "ALG_Akt", 7);
+			$this->RegisterVariableBoolean("ZP", "AutoZSP", "~Switch", 7);
 			$this->RegisterVariableBoolean("Pa", "Party", "~Switch", 8);
 			
 			//___Modulvariabeln______________________________________________________________________
