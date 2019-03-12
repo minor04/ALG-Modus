607282441f55636d345a0002b9112690a8d84fdc
minor04 <46103212+minor04@users.noreply.github.com>
2019-03-12 20:40:21 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -374,7 +374,7 @@ class ALGModus extends IPSModule
 	public function Meldung(){
 		
 		$KategorieID_Settings = IPS_GetCategoryIDByName("Konfigurator Instanzen", 0);
-		$InstanzID = IPS_GetInstanceIDByName("WebFront", 0); 42837
+		$InstanzID = IPS_GetInstanceIDByName("WebFront", 0);
 			
 		//WFC_PushNotification(13905, 'Warnung', 'Test', '', 0);
 		//WFC_PushNotification(42837, 'Warnung', 'Test', '', 0);
