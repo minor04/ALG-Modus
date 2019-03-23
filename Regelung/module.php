ef4dbc960f5f6a711d157b2c60a0325e2a302bd7
minor04 <46103212+minor04@users.noreply.github.com>
2019-03-23 19:34:19 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -342,12 +342,13 @@ class ALGModus extends IPSModule
 		}
 
 		else{
-			IPS_SetHidden($this->GetIDForIdent("Mod"), true);
 			IPS_SetHidden($this->GetIDForIdent("MD"), true);
 			IPS_SetHidden($this->GetIDForIdent("HZ"), true);
 			IPS_SetHidden($VariabelID_Ab, true);
 			IPS_SetHidden($VariabelID_An, true);
-
+			
+			SetValue($this->GetIDForIdent("ZP"), false);
+			SetValue($this->GetIDForIdent("Pa"), false);
 			SetValue($this->ReadPropertyInteger("ALG_HE"), false);
 		}
 		
