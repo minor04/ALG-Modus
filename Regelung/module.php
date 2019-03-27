4d58eb521f295a56d1d6a0b7b3759186bb9acaaf
minor04 <46103212+minor04@users.noreply.github.com>
2019-03-27 15:59:52 +0100

    Update module.php

--- Regelung/module.php
+++ Regelung/module.php
@@ -340,12 +340,12 @@ class ALGModus extends IPSModule
 				IPS_SetHidden($VariabelID_An, false);
 				
 				//if($hz == true && $zp == true){
-					//SetValue($this->ReadPropertyInteger("ALG_HE"), true);
+					SetValue($this->ReadPropertyInteger("ALG_HE"), true);
 					//SetValue($this->GetIDForIdent("Mod"), 2);
 					//IPS_SetHidden($this->GetIDForIdent("Mod"), false);
 				//}
 				//else{
-					//SetValue($this->ReadPropertyInteger("ALG_HE"), false);
+					SetValue($this->ReadPropertyInteger("ALG_HE"), false);
 					//IPS_SetHidden($this->GetIDForIdent("Mod"), true);
 				//}
 			}
